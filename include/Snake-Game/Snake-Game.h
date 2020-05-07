811e2c836614a957329dac9d270756c107682e5f
Sang-geon Yun <c0510gy@protonmail.ch>
2020-05-07 21:35:58 +0900

    add: add prototypes on Snake-Game.h

--- include/Snake-Game/Snake-Game.h
+++ include/Snake-Game/Snake-Game.h
@@ -6,5 +6,89 @@
 #ifndef SNAKE_GAME
 #define SNAKE_GAME
 
+#include <vector>
+#include <queue>
+#include <set>
+
+struct Point{
+    int x, y;
+    Point(int x=0, int y=0){
+        this->x = x;
+        this->y = y;
+    }
+    bool operator<(const Point& p) const{
+        if(x == p.x)
+            return y < p.y;
+        return x < p.x;
+    }
+    bool operator>(const Point& p) const{
+        if(x == p.x)
+            return y > p.y;
+        return x > p.x;
+    }
+    bool operator==(const Point& p) const{
+        return x == p.x && y == p.y;
+    }
+};
+
+enum Item{
+    EMPTY,
+    WALL,
+    IMWAll,
+    GATE,
+    GROWTH,
+    POISON,
+    SANKE
+};
+
+int DIR[4][2] = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+
+class Snake{
+private:
+    int length = 3;
+    int direction = 0;
+    std::vector<Point> snakeBody;
+public:
+    Snake();
+
+    bool move();
+    void growth();
+    void poison();
+    void portal(int x, int y);
+
+    int getDirection();
+    void setDirection(int direction);
+};
+
+class GameRunner{
+private:
+    std::vector<std::vector<Item>> gameMap;
+    Snake snake;
+
+    int frams = 0;
+
+    const int ITEM_MAX_TIME = 10;
+    const int MAX_NUMBER_OF_GROWTH = 3;
+    const int MAX_NUMBER_OF_POISON = 3;
+
+    std::vector<Point> itemsCandidates;
+    std::vector<Point> portalCandidates;
+
+    std::queue<std::pair<Point, int>> growthTimeQ, poisonTimeQ;
+    std::multiset<Point> itemsDeleted;
+    int numberOfGrowth = 0;
+    int numberOfPoison = 0;
+
+    int portalTime = -1;
+    std::vector<Point> portals;
+
+    void updateGrowth();
+    void updatePoison();
+    void updatePortal();
+public:
+    GameRunner(const std::vector<std::vector<Item>>& gameMap);
+
+    bool nextFrame();
+};
 
 #endif
