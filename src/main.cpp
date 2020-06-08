5bbb9f3aeb2548abc3e9c687457bb6cd7b944cc2
KooHyongMo <chrisais9@naver.com>
2020-06-08 18:10:32 +0900

    주석 추가

--- src/main.cpp
+++ src/main.cpp
@@ -42,16 +42,18 @@ int main(int argc, char *argv[])
     GameRunner myGame(myMap, startPoint);
     const MapManager nowMap = myGame.getMap();
 
-    initscr();
-    noecho();
+    initscr(); // ncurses 시작
+    noecho(); // 커서 blink 없이
     curs_set(0);
-    nodelay(stdscr,TRUE);
-    keypad(stdscr, true);
+    nodelay(stdscr,TRUE); // 입력 대기 없이(continuous 하게 게임 진행)
+    keypad(stdscr, true); // 화살표 입력 받기 위해 
 
+    // 색 사용하기 위해
     start_color();
     use_default_colors();
 
     if (has_colors() == FALSE) {
+        // 색 지원 안할때 처리
         endwin();
         printf("Your terminal does not support color\n");
         exit(1);
@@ -62,6 +64,8 @@ int main(int argc, char *argv[])
     init_pair(2, COLOR_GREEN, -1); // snake
     init_pair(3, COLOR_WHITE, COLOR_MAGENTA); // gate
 
+
+    // 화면 크기가 충분하지 않을때 처리
     int maxHeight, maxWidth;
     getmaxyx(stdscr, maxHeight, maxWidth);
     if (nowMap.height > maxHeight || nowMap.width > maxWidth)
@@ -75,6 +79,7 @@ int main(int argc, char *argv[])
 
     while (1)
     {
+        //1. 입력 받고
         int input = getch();
         switch (input)
         {
@@ -98,12 +103,19 @@ int main(int argc, char *argv[])
                 break;
         }
 
+        printw("%d",direction);
+        // 2. 맵 받아오고
         const MapManager nowMap = myGame.getMap();
+
+        // 3. 그려주고
         for (int y = 0; y < nowMap.height; ++y)
         {
             for (int x = 0; x < nowMap.width; ++x)
             {
-                move(x, y);
+                // 커서 위치를 x,y로 이동 시킴.
+                // ncurses를 활용해 글을 쓰려면 다음 순서를 거쳐야함
+                // 1. 커서 이동(move) 2. write(addch)
+                move(x, y); 
                 switch (nowMap.get(x, y))
                 {
                 case EMPTY:
@@ -111,8 +123,11 @@ int main(int argc, char *argv[])
                     break;
                 case WALL:
                 case IMWALL:
+                    // attron : 색 활성화
                     attron(COLOR_PAIR(1));
+                    // addch : 현재 위치에 (위에서 활성화한 색을 가진) 문자 출력
                     addch(' ');
+                    // attroff : 색 비활성화
                     attroff(COLOR_PAIR(1));
                     break;
                 case SNAKE:
@@ -136,15 +151,22 @@ int main(int argc, char *argv[])
                 }
             }
         }
+        //refresh를 invoke 해줘야 ncurses가 화면에 그려줌
         refresh();
         if(!myGame.nextFrame(direction)) {
+            // 종료됨
             printw("breaked");
             refresh();
             break;
         }
+        // 딜레이 안주면 게임이 너무 빨리 진행됨.
+        // 프레임 밀리는 현상이 이 함수 관련한거 같음
+        // 아마 다른 방식으로 스레드에 락 주는 방식을 사용해야 할 것 같음
         usleep(500000);
     }
 
+    // 아래는 프로그램 종료 로직
+    // 이런식으로 종료 안하면 콘솔이 망가짐.
     nodelay(stdscr, false);
 	getch();
     endwin();
