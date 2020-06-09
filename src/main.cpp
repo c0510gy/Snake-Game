041a9c75427d942753589b0777edc9e553214f85
KooHyongMo <chrisais9@naver.com>
2020-06-09 22:13:43 +0900

    FileManager랑 연결

--- src/main.cpp
+++ src/main.cpp
@@ -10,40 +10,14 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    ifstream inFile;
-    inFile.open("./map.txt");
-    Point startPoint;
-    int width, height;
-    inFile >> width >> height;
-    MapManager myMap(width, height);
-    inFile >> startPoint.x >> startPoint.y;
-    for (int y = 0; y < myMap.height; ++y)
-    {
-        string str;
-        inFile >> str;
-        for (int x = 0; x < myMap.width; ++x)
-        {
-            int v = str[x] - '0';
-            switch (v)
-            {
-            case 0:
-                myMap.set(x, y, EMPTY);
-                break;
-            case 1:
-                myMap.set(x, y, WALL);
-                break;
-            case 2:
-                myMap.set(x, y, IMWALL);
-                break;
-            }
-        }
-    }
-    cout << width << " " << height << endl;
+    FileManager test;
+    MapItem items = test.readMap("./testMap2.txt");
+    UserItem users = test.readUser("hi");
 
+    GameRunner myGame(items.gameMap, items.startPoint);
+    GameManager gameManager = GameManager(myGame);
 
-    GameRunner myGame(myMap, startPoint);
-    GameManager gameManger = GameManager(myGame);
-    gameManger.play();
+    gameManager.play();
 
     return 0;
 }
