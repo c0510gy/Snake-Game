8f57d7e98e0bd03c5f44b2b7e4fba1629b302366
Sang-geon Yun <c0510gy@protonmail.ch>
2020-06-12 16:17:31 +0900

    implement: implement backpropagation algorithm on MLP

--- include/GA-MLP/MLP.h
+++ include/GA-MLP/MLP.h
@@ -10,7 +10,7 @@
 #include <vector>
 #include <cmath>
 
-template <class ActivationFunction>
+template <class ActivationFunction, class dActivationFunction>
 class Perceptron{
 private:
     int numberOfWeights;
@@ -18,25 +18,33 @@ private:
     long double bias;
 
     long double out;
+    long double error;
 
     long double activationFunction(long double x);
+    long double dActivationFunction(long double x);
 public:
     Perceptron(int numberOfWeights=0);
 
     long double calc(const std::vector<long double>& inputs);
-    long double get();
+    long double getCalc();
+
+    void gradientDescent(const std::vector<long double>& inputs, long double learningRate=0.0015);
+    void setError(long double error); // 현재 퍼셉트론에 대한 에러 설정
+    long double getNextError(int inputIdx); // 이전 레이어의 inputIdx 퍼셉트론에 대한 에러 반환
 };
 
-template <class ActivationFunction>
+template <class ActivationFunction, class dActivationFunction>
 class MLP{
 private:
     int inputLayerNodes, hiddenLayers, outputLayerNodes;
-    std::vector<std::vector<Perceptron<ActivationFunction>>> network;
+    std::vector<long double> inputLayer;
+    std::vector<std::vector<Perceptron<ActivationFunction, dActivationFunction>>> network;
 
 public:
     MLP(const std::vector<int>& eachLayer);
 
     std::vector<long double>& run(const std::vector<long double>& inputs);
+    void backpropagation(const std::vector<long double>& errors, long double learningRate=0.0015);
 };
 
 #endif
--- src/MLP.cpp
+++ src/MLP.cpp
@@ -5,34 +5,52 @@
 
 #include "MLP.h"
 
-template <class ActivationFunction>
-Perceptron<ActivationFunction>::Perceptron(int numberOfWeights){
+template <class ActivationFunction, class dActivationFunction>
+Perceptron<ActivationFunction, dActivationFunction>::Perceptron(int numberOfWeights){
     this->numberOfWeights = numberOfWeights;
     wieghts.resize(numberOfWeights);
 }
-template <class ActivationFunction>
-long double Perceptron<ActivationFunction>::activationFunction(long double x){
+template <class ActivationFunction, class dActivationFunction>
+long double Perceptron<ActivationFunction, dActivationFunction>::activationFunction(long double x){
     return ActivationFunction()(x);
+}template <class ActivationFunction, class dActivationFunction>
+long double Perceptron<ActivationFunction, dActivationFunction>::dActivationFunction(long double x){
+    return dActivationFunction()(x);
 }
-template <class ActivationFunction>
-long double Perceptron<ActivationFunction>::calc(const std::vector<long double>& inputs){
+template <class ActivationFunction, class dActivationFunction>
+long double Perceptron<ActivationFunction, dActivationFunction>::calc(const std::vector<long double>& inputs){
     out = bias;
     for(int j = 0; j < numberOfWeights; ++j)
         out += inputs[j] * weights[j];
     out = activationFunction(out);
     return out;
 }
-template <class ActivationFunction>
-long double Perceptron<ActivationFunction>::get(){
+template <class ActivationFunction, class dActivationFunction>
+long double Perceptron<ActivationFunction, dActivationFunction>::getCalc(){
     return out;
 }
+template <class ActivationFunction, class dActivationFunction>
+void Perceptron<ActivationFunction, dActivationFunction>::gradientDescent(const std::vector<long double>& inputs, long double learningRate){
+    for(int j = 0; j < numberOfWeights; ++j)
+        weights[j] -= inputs[j] * dActivationFunction(getCalc()) * error * learningRate;
+    bias -= dActivationFunction(getCalc()) * error * learningRate;
+}
+template <class ActivationFunction, class dActivationFunction>
+void Perceptron<ActivationFunction, dActivationFunction>::setError(long double error){
+    this->error = error;
+}
+template <class ActivationFunction, class dActivationFunction>
+long double Perceptron<ActivationFunction, dActivationFunction>::getNextError(int inputIdx){
+    return weights[inputIdx] * dActivationFunction(getCalc()) * error;
+}
 
-template <class ActivationFunction>
-MLP<ActivationFunction>::MLP(const std::vector<int>& eachLayer){
+template <class ActivationFunction, class dActivationFunction>
+MLP<ActivationFunction, dActivationFunction>::MLP(const std::vector<int>& eachLayer){
     this->inputLayerNodes = eachLayer.front().size();
     this->hiddenLayers = eachLayer.size() - 2;
     this->outputLayerNodes = eachLayer.back().size();
     network.resize(hiddenLayers + 1);
+    inputLayer.resize(inputLayerNodes);
 
     int preNodes = inputLayerNodes;
     for(int layer = 0; layer < hiddenLayers + 1; ++layer){
@@ -41,8 +59,10 @@ MLP<ActivationFunction>::MLP(const std::vector<int>& eachLayer){
         preNodes = eachLayer[layer];
     }
 }
-template <class ActivationFunction>
-std::vector<long double>& MLP<ActivationFunction>::run(const std::vector<long double>& inputs){
+template <class ActivationFunction, class dActivationFunction>
+std::vector<long double>& MLP<ActivationFunction, dActivationFunction>::run(const std::vector<long double>& inputs){
+    for(int p = 0; p < inputLayerNodes; ++p)
+        inputLayer[p] = inputs[p];
     for(int p = 0; p < network[0].size(); ++p)
         network[0][p].calc(inputs);
     for(int layer = 1; layer < hiddenLayers + 1; ++layer){
@@ -54,6 +74,33 @@ std::vector<long double>& MLP<ActivationFunction>::run(const std::vector<long do
     }
     std::vector<long double> outputs;
     for(int p = 0; p < outputLayerNodes; ++p)
-        outputs.push_back(network.back()[p].get());
+        outputs.push_back(network.back()[p].getCalc());
     return outputs;
+}
+template <class ActivationFunction, class dActivationFunction>
+void MLP<ActivationFunction, dActivationFunction>::backpropagation(const std::vector<long double>& errors, long double learningRate){
+    std::vector<long double> nextErrors = errors;
+    for(int layer = hiddenLayers; layer >= 0; --layer){
+        std::vector<long double> tempErrors, inputs;
+        if(layer){
+            tempErrors.resize(network[layer - 1].size());
+            for(int p = 0; p < network[layer - 1].size(); ++p)
+                inputs.push_back(network[layer - 1][p].getCalc());
+        }else{
+            for(int p = 0; p < inputLayerNodes; ++p)
+                inputs.push_back(inputLayer[p]);
+        }
+        for(int p = 0; p < network[layer].size(); ++p){
+            network[layer][p].setError(nextErrors[p]);
+
+            if(layer)
+                for(int np = 0; np < network[layer - 1].size(); ++np)
+                    tempErrors[np] += network[layer][p].getNextError(np);
+            
+            network[layer][p].gradientDescent(inputs, learningRate);
+        }
+        nextErrors = tempErrors;
+        for(int j = 0; j < nextErrors.size(); ++j)
+            nextErrors[j] /= network[layer].size();
+    }
 }
